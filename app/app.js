161ea1542de812db1488d8f0c3661bcd3fc25478
guokaixuan <guokaixuan@xiaomuzhi.com>
2017-03-02 15:47:10 +0800

    Upload resume

---
+++ app/app.js
@@ -0,0 +1,253 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StyleEditor from "./StyleEditor.js";
+import ResumeEditor from "./ResumeEditor.js";
+import "./style/reset.css";
+import Prism from "prismjs";
+import co from "co";
+
+class ReactClass extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			style: "",
+		};
+		this.interval = 40;
+		this.resumeEditorContent = `
+# 郭凯旋
+
+
+## 项目及工作经历
+
+### 移动端开发主管
+* 杭州小拇指汽车维修科技股份有限公司
+* 2016 年 2 月 - 至今 
+* 管理移动端开发团队，参与产品前期设计及建议。
+* 完成全国700+门店全部使用的目标，实现SA订单转化率计算与高毛利业务的云推送模块；
+* 帮助店主实现对门店的日常管理及财务报表统计。
+* 负责公司C端项目研发、架构设计，提供iOS框架代码及功能组件。
+* 优化产品的性能及奔溃信息的记录解决，使用react-native实现客户端的跨平台开发。
+
+### iOS开发工程师
+* 658金融网
+* 2015 年 7 月 - 2016 年 2 月 
+* 开发及维护公司金融理财App产品，后期负责独立开发一款直播App。
+* 对公司已有项目进行代码优化和项目提交，公司系统框架搭建，管理iOS小组开发人员 。
+* 金融理财App完成两次架构及UI迭代升级，appstore下载量突破50W+。
+* 直播App实现独立的即时通讯长链接服务，完成直播服务接入及测试上线。
+
+### iOS开发工程师
+* 上海东方传媒公司(宝盈股份)
+* 2014 年 7 月 - 2015 年 6 月
+* 负责手机软件开发，App高级界面搭建设计，业务逻辑开发，参与后台数据接口集成。
+* 基于Visual Studio进行企业管理软件开发，参与服务平台及应用软件开发。
+* 完成公司要求的手机软件设计、编码和测试工作。搭建企业内部通信系统及网络维护工作。
+
+
+
+## 博客
+
+**GitHub: **https://github.com/softAlexs
+
+> 如果你喜欢这个效果，Fork [我的项目](https://github.com/softAlexs/softalexs.github.io)，打造你自己的简历！`;
+		
+		this.styleEditorContent = [`/*
+* Inspired by http://strml.net/
+* 
+* Hello, 我是郭凯旋
+*
+* 最近看到了别人用 Vue 做了一个动态的简历，感觉很有意思，所以我也用 React 做了一份简易的动态简历
+* 希望大家能够喜欢 :)
+*/
+
+/* 所以我们就开始吧！首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: #425261; 
+}
+/* 文字直接显示在页面上，没有任何装饰，真的人反人类呢！所以我们来给文字加点装饰吧~~ */
+.styleEditor {
+  background-color: #303030;
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
+/* 作为一个程序员，我们不可以太沉闷哦~~，给自己的代码加一点色彩吧 */
+.token.comment{ color: #857F6B; font-style: italic; }
+.token.selector{ color: #E86E75; }
+.token.property{ color: #F78C6C; }
+.token.punctuation{ color: #88DCFE; }
+.token.function{ color: #82AAFF; }
+
+/* 加一点 3D 效果，更加地酷炫 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0; 
+  -webkit-transition: none; 
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+/* 不知道以上对代码框的修改你是否喜欢呢？ */
+
+/* 接下来我给自己准备一个编辑器，用来存放我的简历内容 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh; 
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+
+/* 好了，我开始写简历了 */
+`,
+`
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ *           3          
+ *           2          
+ *           1          
+ *          啦啦！
+ */
+`,
+`
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h1{
+  display: block;
+  width: 80px;
+  margin: 0 auto;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor h3{
+	display: inline-block;
+	margin: 0.5em 0;
+}
+.resumeEditor a{
+	color: #000;
+}
+.resumeEditor ul{
+	list-style: none;
+}
+.resumeEditor ul>li::before {
+	content: "•";
+	margin-left: 1em;
+	margin-right: 0.5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+/*
+* 做自己喜欢的事，人生能有多少机会这么不顾一切呢？
+* I hope you enjoyed this.
+*/
+`];
+	}
+
+	addToStyle(char) {
+		this.setState({
+			style: this.state.style + char,
+		});
+	}
+
+	replaceStyle(style) {
+		this.setState({
+			style: style,
+		});
+	}
+
+	replaceStyleEditorContent() {
+		
+	}
+
+	showStyleEditorContent(n) {
+		let lastContentLength = 0;
+		if (n !== 0) {lastContentLength = this.state.style.length;}
+		let style = this.styleEditorContent[n];
+		let len = style.length;
+		return new Promise((resolve, reject) => {
+			let showStyle = function () {
+				let currentLen = this.state.style.length - lastContentLength;
+				if (currentLen < len) {
+					let char = style.substring(currentLen, currentLen+1);
+					this.refs.StyleEditor.addToContent(char);
+					this.addToStyle(char);
+					setTimeout(showStyle, this.interval);
+				} else {
+					resolve();
+				}
+			}.bind(this);
+			showStyle();
+		});
+	}
+
+	showResumeContent() {
+		let content = this.resumeEditorContent;
+		let len = content.length;
+		return new Promise((resolve, reject) => {
+			let showContent = function() {
+				let currentLen = this.refs.ResumeEditor.getCurrentContentLength();
+				if (currentLen < len) {
+					let char = content.substring(currentLen, currentLen+1);
+					this.refs.ResumeEditor.addToContent(char);
+					setTimeout(showContent, this.interval);
+				} else {
+					resolve();
+				}
+			}.bind(this);
+			showContent();
+		});
+	}
+
+	setResumeMarkdown() {
+		return new Promise((resolve, reject) => {
+			setTimeout(this.refs.ResumeEditor.setIsMarkdown(true), this.interval);
+			resolve();
+		}); 
+	}
+
+	async startShow() {
+		await this.showStyleEditorContent(0).then(function() {console.log('done! show Content 0')});
+		await this.showResumeContent();
+		await this.showStyleEditorContent(1).then(function() {console.log('done! show Content 1')});
+		await this.setResumeMarkdown();
+		await this.showStyleEditorContent(2).then(function() {console.log('done! show Content 2')});
+	}
+
+	componentDidMount() {
+		this.startShow();
+		console.log(111);
+		// this.refs.StyleEditor.replaceContent(this.content[0]);
+		// this.replaceStyle(this.content[0]); 
+		// this.refs.ResumeEditor.replaceContent("");
+	}
+
+	render() {
+		return (
+			<div>
+				<StyleEditor ref="StyleEditor" />
+				<ResumeEditor ref="ResumeEditor" />
+				<style>{this.state.style}</style>
+			</div>);
+	}
+}
+ReactDOM.render(<ReactClass />, document.getElementById("content"));
